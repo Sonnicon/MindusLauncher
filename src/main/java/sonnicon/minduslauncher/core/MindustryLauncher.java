4b5b88e8aff7653defbe3cf61faab4fe24f0150a
Sonnicon <sonniconn@gmail.com>
2020-10-01 20:36:02 +0100

    Basic loadermods

--- src/main/java/sonnicon/minduslauncher/core/MindustryLauncher.java
+++ src/main/java/sonnicon/minduslauncher/core/MindustryLauncher.java
@@ -1,11 +1,12 @@
 package sonnicon.minduslauncher.core;
 
 import java.io.File;
+import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.jar.JarFile;
 
-//todo loader mods
 public class MindustryLauncher{
     public static void main(String[] args){
         if(args.length < 1) return;
@@ -14,7 +15,26 @@ public class MindustryLauncher{
             JarFile jar = new JarFile(f);
             String mainClass = jar.getManifest().getMainAttributes().getValue("Main-Class");
             jar.close();
-            Class c = Class.forName(mainClass, true, new URLClassLoader(new java.net.URL[]{f.toURI().toURL()}));
+
+            ArrayList<URL> urls = new ArrayList<>();
+            urls.add(f.toURI().toURL());
+
+
+            File moddir = new File(f.getParent(), "loadermods");
+            if(!moddir.exists()) moddir.mkdirs();
+            for(File mod : moddir.listFiles()){
+                urls.add(mod.toURI().toURL());
+            }
+            URLClassLoader classloader = new URLClassLoader(urls.toArray(new URL[]{}));
+
+            for (File mod : moddir.listFiles()){
+                JarFile modjar = new JarFile(mod);
+                classloader.loadClass(modjar.getManifest().getMainAttributes().getValue("Main-Class"))
+                        .getMethod("main", String[].class).invoke(null, new Object[]{new String[]{}});
+                modjar.close();
+            }
+
+            Class c = Class.forName(mainClass, true, classloader);
             c.getMethod("main", String[].class).invoke(null, new Object[]{Arrays.copyOfRange(args, 1, args.length)});
         }catch(Exception ex){
             ex.printStackTrace();
